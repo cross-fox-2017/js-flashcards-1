eb93ce4af20edef797f9c646118b461615bb48ef
Izhharuddin Syanmil & Timothy Ombun Giovanni Siahaan <pair+syanmil.timocodex@hacktiv8.com>
2017-01-13 10:13:54 +0700

    parsing data selesai

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,9 @@
 "use strict"
-// write your code here
+const fs = require("fs")
+
+function parse(data){
+  let parsed = JSON.parse(fs.readFileSync(data,'utf-8'))
+  return parsed
+}
+
+console.log(parse('data.json')[0].term)
