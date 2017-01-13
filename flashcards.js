aa58c9ec2fee32efb8663ddb9872c9fcd14a2e01
Ida Bagus Chahya Dhegana & Fadly Kayo <pair+dgana.fadlykayo@hacktiv8.com>
2017-01-13 11:48:43 +0700

    dgana fadly done all release

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,30 @@
 "use strict"
-// write your code here
+
+let fs = require("fs");
+let readData = JSON.parse(fs.readFileSync("data.json", "utf8"));
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let count = 0
+
+rl.setPrompt(`Welcome to JS Flash Cards. To play, just enter the correct term for each definition. Ready? Go!\n\nDefinition\n${readData[0].definition}\n\nGuess: `)
+rl.prompt();
+
+rl.on('line', (answer) => {
+  if(answer.trim().toLowerCase() === readData[5].term.toLowerCase()){
+    console.log('Selamat jawaban anda betul semua!');
+    rl.close();
+  } else if(answer.trim().toLowerCase() === readData[count].term.toLowerCase()){
+    console.log('Correct!');
+    rl.setPrompt(`\nDefinition:\n${readData[count+1].definition}\n\nGuess: `);
+    rl.prompt();
+    count++;
+  } else {
+    console.log("Incorrect! Try again.");
+    rl.prompt();
+  }
+});
