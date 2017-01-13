8b32d69bebe1a9e114f984f5e1dffcd863d22a8b
Muhammad Iqbal & yoma sofwan <pair+qblol.yomaswn@hacktiv8.com>
2017-01-13 11:14:22 +0700

    done

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,36 @@
 "use strict"
 // write your code here
+const fs = require('fs');
+let obj = require('./data.json');
+let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: ''
+});
+
+rl.prompt();
+var x = 0
+console.log(data[x].definition)
+rl.on('line', (line) => {
+  if (line.trim().toLowerCase() == data[x].term.toLowerCase()) {
+      console.log('Jawaban Benar!');
+      x+=1
+      if (x < data.length) {
+        console.log(data[x].definition)
+      } else {
+        console.log("WAH HEBAT BGT LU")
+        process.exit(0);
+      }
+
+  } else {
+    console.log(`Jawaban '${line.trim()} masih salah'`);
+    console.log(data[x].definition)
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Ah Culun berenti tengah jalan');
+  process.exit(0);
+});
