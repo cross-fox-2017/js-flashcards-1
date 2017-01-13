c25aa2443a2a7af54e5cc90df1abb7c094ae8958
Muhammad Firman Pebrizal & Eri Irawan <pair+mfpebrizal.mrei91@hacktiv8.com>
2017-01-13 13:44:27 +0700

    Done

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,37 @@
 "use strict"
 // write your code here
+let fs = require('fs');
+let i = 0;
+var  question = JSON.parse(fs.readFileSync('data.json','utf-8'))
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+console.log('Welcome to JS Flash Card. To play, just enter the correct term for each definition. Ready ? GO ! \n')
+
+rl.setPrompt(`${question[i].definition}?\nJawaban :`)
+rl.prompt()
+
+rl.on('line', (answer) => {
+  if((i+1)<question.length){
+    switch(answer.trim().toUpperCase()) {
+      case question[i].term.toUpperCase():
+        console.log('jawaban benar');
+        i++
+        break;
+      default:
+        console.log(`jawaban salah`);
+        break;
+    }
+    rl.setPrompt(`${question[i].definition}?\nJawaban :`)
+    rl.prompt();
+  }else{
+    rl.close()
+  }
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
