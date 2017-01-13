87c851bce9f4946a994c2b2515a10571efaf5146
Izhharuddin Syanmil & Timothy Ombun Giovanni Siahaan <pair+syanmil.timocodex@hacktiv8.com>
2017-01-13 10:52:54 +0700

    mvc ready

--- flashcards.js
+++ flashcards.js
@@ -1,9 +1,54 @@
 "use strict"
-const fs = require("fs")
+const fs = require("fs");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt : ' '
+});
 
-function parse(data){
-  let parsed = JSON.parse(fs.readFileSync(data,'utf-8'))
-  return parsed
+class Model{
+  constructor(){
+    this.data = data
+  }
+  static parse(data){
+    let parsed = JSON.parse(fs.readFileSync(data,'utf-8'))
+    return this.data = parsed
+  }
 }
 
-console.log(parse('data.json')[0].term)
+class View{
+
+  static permainan(){
+    console.log(`Selamat datang Dipermaian FlashCard 1\n\n`);
+    let counter = 0
+    rl.setPrompt(`${Model.data[counter].definition}?\n`)
+    rl.prompt();
+    rl.on('line', (jawaban) => {
+      if(jawaban == Model.data[counter].term){
+        console.log(`Jawaban Anda BETUL!!! \n`);
+        counter++
+      }else{
+        console.log(`Yah, masih salah, coba lagi bro \n`);
+      }
+      if(counter=== Model.data.length){
+        rl.close()
+      }
+      rl.setPrompt(`${Model.data[counter].definition}? \n`)
+      rl.prompt();
+    }).on('close', () => {
+      console.log('Hebat coy!');
+      process.exit(0);
+    });
+  }
+}
+
+class Controller{
+  static run(){
+    Model.parse('data.json');
+    View.permainan();
+    //return `Permainan selesai anda menanggg`
+  }
+}
+
+console.log(Controller.run())
